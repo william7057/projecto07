bd27ddd435dcd28596dd070dc8ae6d55f140a448
william <factzure@hotmail.com>
2021-09-08 12:04:29 -0500

    Agregar archivos de proyecto.

---
+++ projecto07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projecto07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 2;
+            Console.Write(1);
+            while (x<=10) 
+            {
+                Console.Write(" , ");
+                Console.Write(x);
+                x = x + 1;
+            }
+
+
+            Console.Read();
+
+        }
+    }
+}
